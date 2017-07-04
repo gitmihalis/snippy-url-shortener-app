758ed258ca6b0205ba674207ec9f98d6e7883497
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-07-04 14:48:46 -0400

    pass data to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -7,12 +7,18 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase)
+})
+
+app.get('/urls', (req, res) => {
+  res.render('urls_index', { urls: urlDatabase } );
 })
 
 app.get('/html', (req, res) => {
