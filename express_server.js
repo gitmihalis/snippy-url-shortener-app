7087503b89034dcb0e6387efb711e9651b35034a
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-07-04 14:05:32 -0400

    implement multiple request routes

---
+++ express_server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8888; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+})
+
+app.get('/html', (req, res) => {
+  res.end("<html><body>Can you dig it, <b> doOoDe?</b></body></html>")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
