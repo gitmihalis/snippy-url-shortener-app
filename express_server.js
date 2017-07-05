64b39b12fffc504b4fc8b08a35af00efe2313615
gitmihalis <mihalisfthenos@icloud.com>
2017-07-05 16:21:41 +0000

    delete a resource

--- express_server.js
+++ express_server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-
-const PORT = process.env.PORT || 8888; // default port 8080
+const bodyParser = require("body-parser");
+const PORT = 8080;
 
 function generateRandomString() {
     let = randomString = "";
@@ -12,10 +11,9 @@ function generateRandomString() {
     }
     return randomString;
 }
-generateRandomString();
 
 var urlDatabase = {
-  "nstgrm": "http://www.instagram.com",
+  "abcdef": "http://www.instagram.com",
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -27,38 +25,55 @@ app.use(bodyParser.urlencoded( {extended: true} ));
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
-});
+
 app.get('/urls', (req, res) => {
   res.render('urls_index', { urls: urlDatabase } );
 });
+
 // get new url form
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
-app.post("/urls", (req, res) => {
-  // console.log(` req.body.longURL: ${req.body.longURL}`);  // debug statement to see POST parameters
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
+
+// show url
 app.get('/urls/:id', (req, res) => {
   const url = {
     short: req.params.id,
     long: urlDatabase[req.params.id] };
+  if ( urlDatabase[req.params.id] )
   res.render('urls_show', { url }  );
 });
+
 // handle shortURL requests:
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+app.post("/urls", (req, res) => {
+  // console.log(` req.body.longURL: ${req.body.longURL}`);  // debug statement to see POST parameters
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");        
+   // Respond with 'Ok' (we will replace this)
+});
 
 
+app.post('/urls/:id/delete', (req, res) => {
+  // delete the resouce
+  let id = req.params.id;
+  delete urlDatabase[id];
+  console.log(urlDatabase)
+  // redirect user to urls_index
+  res.redirect('/urls');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Go to http://localhost:${PORT} in your browser to see Tiny App.`);
 });
