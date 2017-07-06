6c5b8dee1403145e0639b1523eb146fdafbbf42d
gitmihalis <mihalisfthenos@icloud.com>
2017-07-06 17:30:44 +0000

    create login page

--- express_server.js
+++ express_server.js
@@ -80,9 +80,8 @@ app.post('/urls/:id', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.send('OK')
-});
-
+  res.render('login');
+})
 // handle the login form submission
 app.post('/login', (req, res) => {
   const username = req.body.username;
