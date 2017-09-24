52ed33db20622dd784b899d3e56f2afefb3193eb
Mihalis <mihalisfthenos@icloud.com>
2017-09-24 15:21:44 -0400

    add demo user

--- mock-data.js
+++ mock-data.js
@@ -14,6 +14,11 @@ const users = {
     id: "mihaliswastaken", 
     email: "mihalis@mail.com", 
     password: bcrypt.hashSync("foobar", 10),
+  },
+  "demo": {
+    id: "demo",
+    email: "demo@mail.com",
+    password: bcrypt.hashSync("password", 10),
   }
 }
 
@@ -21,11 +26,13 @@ const urls = {
   "abcdef": { long: "http://www.instagram.com", userID: "userRandomID", views: { unique: 0, total: 0 } },
   "b2xVn2": { long: "http://www.lighthouselabs.ca", userID: "userRandomID", views: { unique: 0, total: 0 } },
   "9sm5xK": { long: "http://www.google.com", userID: "mihaliswastaken", views: { unique: 0, total: 0 } },
+  "ghijkl": { long: "http://duckduckgo.com", userID: "demo", views: { unique: 733, total: 984} }
 };
 
 const visits = {
   "abcdef": [{ visitor_id: "E3rds4", timestamp: Date.now() - 2122 }],
   "9sm5xK": [{ visitor_id: "r5tdf9", timestamp: Date.now() - 189282 }],
+  "ghijkl": [{ visitor_id: "rsf08d", timestamp: Date.now() - 199282 }]
 };
 
 module.exports = { urls, users, visits };
